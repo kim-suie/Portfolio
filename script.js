efa06cd914216706608fa5030f684e29c6835d79
kim-suie <kim1998suie@gmail.com>
2026-04-01 13:59:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -21,3 +21,10 @@ window.onscroll = () => {
         }
     });
 };
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    navbar.classList.toggle('active');
+};
